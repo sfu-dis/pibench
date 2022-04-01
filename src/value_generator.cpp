c3a97ab5f935e37f09e0746ae1bcdb005b66586a
Ge Shi <utashih@gmail.com>
2022-04-01 06:34:40 -0700

    Load and verify values with hashed keys
    This commit works if key and value are both 8-byte.

--- src/value_generator.cpp
+++ src/value_generator.cpp
@@ -1,6 +1,19 @@
 #include "value_generator.hpp"
 
+#include <cassert>
+#include <cstring>
+
+#include "utils.hpp"
+
 namespace PiBench
 {
 thread_local foedus::assorted::UniformRandom value_generator_t::rng_;
+thread_local char value_generator_t::buf_[KEY_MAX];
+
+const char* value_generator_t::from_key(uint64_t key)
+{
+    auto hashed = utils::multiplicative_hash<uint64_t>(key);
+    memcpy(buf_, &hashed, sizeof(uint64_t));
+    return buf_;
+}
 }
